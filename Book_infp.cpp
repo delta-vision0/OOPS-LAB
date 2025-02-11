6a688da491cd330d16c331eba26519b4c979a691
delta-vision0 <navanathbhosle29@gmail.com>
2025-02-10 21:28:56 -0800

    Update Book_infp.cpp

--- Book_infp.cpp
+++ Book_infp.cpp
@@ -11,17 +11,18 @@ class Book{
 	   int chapter_number;
 	public:
 		Book(){
+		    cout<<"default constructor"<<endl;
 			Title=new char[1];
 			Title[0]='\0';
 			Author=new char[1];
 			Author[0]='\0';
 			page_count=0;
 			format_type=false;
-			chapter_pg=NULL;
+			chapter_pg=0;
 			chapter_number=0;
 		}
 		Book(const char* t, const char* a, int pages, bool format, float* chap_pg,int chap_num)
-		{
+		{   cout<<"parameter constructor"<<endl;
 			Title=new char[strlen(t)+1];
 			strcpy(Title,t);
 			
@@ -30,14 +31,15 @@ class Book{
 			
 			page_count=pages;
 			format_type=format;
+			chapter_number=chap_num;
 			chapter_pg= new float[chap_num];
 			for (int i = 0; i < chap_num; i++) {
             chapter_pg[i] =chap_pg[i];
-            
         }
-			chapter_number=chap_num;
 		}
+		
 		Book(const Book &obj){
+		    cout<<"copy constructor"<<endl;
 			Title=obj.Title;
 			Author=obj.Author;
 			page_count=obj.page_count;
@@ -45,10 +47,13 @@ class Book{
 			chapter_pg=obj.chapter_pg;
 			chapter_number=obj.chapter_number;
 		}
+		
 		void setChapterpagecount(int x){
-			page_count=x;
+			page_count=x; //assigns the new page count
 		}
+		
 		void getinfo(){
+		    //display the information
 			cout<<"Book Name :"<<Title << endl; 
 			cout<<"Author Name :"<<Author <<endl;
 			cout<<"Page Count :"<<page_count<<endl;
@@ -59,19 +64,30 @@ class Book{
 			else{
 				cout<<"NO"<<endl;
 			}
+			cout<<"number of chapters :" <<chapter_number<<endl;
 			cout<<"Chapter Pages :";
 			for(int i=0;i<chapter_number;i++){
 				cout<<chapter_pg[i];
 			    cout<<"  ";
 			}
-			cout<<endl;
-			cout<<"number of chapters :" <<chapter_number<<endl;
+			cout<<endl<<endl;
 		}
+		
+		~Book(){
+			    cout<<"destructor called"<<endl;
+			    delete Title;
+			    delete Author;
+			    delete chapter_pg;
+			    }
 };
 int main() {
 	Book b1;
 	b1.getinfo();
 	float arr[20]={10,11,12,13,14,15,16,17,18,19};
 	Book b2("BOOK","Haris",121,true,arr,10);
+
 	b2.getinfo();
+	Book b3=b2;
+	b3.setChapterpagecount(500);
+	b3.getinfo();
 }
