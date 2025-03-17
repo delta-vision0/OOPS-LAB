06e4f2709935cc998bd912cfffb87eedcb844a23
delta-vision0 <navanathbhosle29@gmail.com>
2025-03-17 15:57:13 +0530

    Update Assignment 4.cpp

--- Assignment 4.cpp
+++ Assignment 4.cpp
@@ -4,15 +4,15 @@ using namespace std;
 class Bank{
 	public:
 		void deposit(int amt){
-			cout<<"Depositing "<<amt<<" in cash"<<endl;
+			cout<<"Depositing Rs."<<amt<<" in cash"<<endl;
 		}
 		
 		void deposit(double amt){
-			cout<<"Depositing "<<amt<<" via digital payment"<<endl;
+			cout<<"Depositing Rs."<<amt<<" via digital payment"<<endl;
 		}
 		
 		void deposit(string UPI_ID,float amt){
-			cout<<"Depositing "<<amt<<" Rs !"<<" Using UPI ("<<UPI_ID<<")"<<endl;
+			cout<<"Depositing Rs."<<amt<<" Using UPI ("<<UPI_ID<<")"<<endl<<endl;
 		}	
 };
 
@@ -28,19 +28,22 @@ class Account {
 		
 		void Display(){
 			cout<<"Account No:"<<accountNumber<<endl;
-			cout<<"Balance:"<<balance<<endl;
+			cout<<"Balance:"<<balance<<endl<<endl;
 		}
 		
 		void operator+(Account &acc){
 			float TransferAmt;
-			cout<<"Enter the amount to transfer from Account"<<accountNumber<<"to Account"<<acc.accountNumber<<endl;
+			cout<<"Account "<<accountNumber<< " Balance : Rs.."<<balance<<endl;
+			cout<<"Account "<<acc.accountNumber<< " Balance : Rs.."<<acc.balance<<endl<<endl;
+			cout<<"Enter the amount to transfer from Account "<<accountNumber<<" to Account "<<acc.accountNumber<<":";
 			cin>>TransferAmt;
+			cout<<endl<<endl;
 			if(balance>=TransferAmt){
 				balance=balance-TransferAmt;
 				acc.balance=acc.balance+TransferAmt;
-				cout<<"Transferring Rs"<<TransferAmt<<"From Account"<<accountNumber<<"To Account"<<acc.accountNumber<<endl;
-				cout<<"New Account"<<accountNumber<<"Balance :"<<balance<<endl;
-				cout<<"New Account"<<acc.accountNumber<<"Balance :"<<acc.balance<<endl;
+				cout<<"Transferring Rs."<<TransferAmt<<" From Account "<<accountNumber<<" To Account "<<acc.accountNumber<<endl;
+				cout<<"New Account "<<accountNumber<<" Balance :Rs."<<balance<<endl;
+				cout<<"New Account "<<acc.accountNumber<<" Balance :Rs."<<acc.balance<<endl<<endl;
 			}
 		}
 		
@@ -48,7 +51,7 @@ class Account {
 			if(balance>=WithdrawAmt)
 			{
 				balance=balance-WithdrawAmt;
-				cout<<"Withdrawing "<<WithdrawAmt<<"from"<<accountNumber<<endl;
+				cout<<"Withdrawing Rs."<<WithdrawAmt<<" from Account "<<accountNumber<<endl;
 				cout<<"Remaining Balance : "<<balance<<endl;
 			}	
 	}
